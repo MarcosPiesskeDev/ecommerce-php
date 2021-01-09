2f7dbe165fc475de6eeb3d30f93c614c418611d0
Marcos V. Piesske <mv.piesske@gmail.com>
2021-01-09 17:35:50 -0300

    Creating first query with Slim framework

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php'; 
+session_start();
+
+use HcodeEcom\db\MethodsDb\MethodsDb;
+
+require __DIR__.'/vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get("/", function(){
+    $conn = new MethodsDb();
+    
+    $results = $conn->select("SELECT * FROM person");
+
+    echo json_encode($results);
+
+});
+
+$app->run();
