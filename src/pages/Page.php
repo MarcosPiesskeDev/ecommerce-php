f1a34589b758a6ac5a2fc0628300ece0c0d31d6a
Marcos V. Piesske <mv.piesske@gmail.com>
2021-01-09 19:01:08 -0300

    Creating teamplate class to create dinamically the pages

---
+++ src/pages/Page.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace HcodeEcom\pages;
+
+use Rain\Tpl;
+
+class Page{
+    
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data" => []
+    ];
+
+    public function setData($data = []){
+        foreach($data as $key => $value){
+            $this->tpl->assign($key, $value);
+        }
+    }
+
+   
+    public function __construct($opts = [])
+    {
+        
+        $this->options = array_merge($this->defaults, $opts);
+        $config = [
+            "tpl_dir"   => $_SERVER['DOCUMENT_ROOT']."/views/",
+            "cache_dir" => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
+            "debug"     => false
+        ];
+       
+        Tpl::configure( $config );
+
+        $this->tpl = new Tpl;
+
+        $this->setData($this->options["data"]);
+
+        $this->tpl->draw("header");
+    }
+ 
+
+    public function setTpl($name, $data = [], $returnHtml = false)
+    {
+        $this->setData($data);
+
+        return $this->tpl->draw($name, $returnHtml);
+    }
+
+    public function __destruct()
+    {
+        $this->tpl->draw("footer");
+    }
+}
