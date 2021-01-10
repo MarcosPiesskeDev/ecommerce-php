0e1c0df5fb28b50fd2ebf1165d6436b2257a2c07
Marcos V. Piesske <mv.piesske@gmail.com>
2021-01-10 17:39:08 -0300

    Creating Admin page class to run template from them

---
+++ src/pages/PageAdmin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HcodeEcom\pages;
+
+
+class PageAdmin extends Page{
+
+    public function __construct($opts = [], $tplDir = '/views/admin/')
+    {
+        parent::__construct($opts, $tplDir);
+    }
+}
