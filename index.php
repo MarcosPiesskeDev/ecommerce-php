d6aafa72fa5f1d475789e4c70f895f2e3d2a60c3
Marcos V. Piesske <mv.piesske@gmail.com>
2021-01-10 17:40:22 -0300

    Creating .htaccess to run in other endpoints (like admin) and add admin endpoint on index

--- index.php
+++ index.php
@@ -5,11 +5,19 @@ session_start();
 require __DIR__.'/vendor/autoload.php';
 
 use HcodeEcom\pages\Page;
+use HcodeEcom\pages\PageAdmin;
 
 $app = new Slim\Slim();
 
 $app->get("/", function(){
    $page = new Page();
+
+   $page->setTpl("index");
+
+});
+
+$app->get("/admin", function(){
+   $page = new PageAdmin();
 
    $page->setTpl("index");
 
