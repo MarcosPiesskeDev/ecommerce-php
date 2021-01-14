aa60d1196e910d0f826db013dc62c461655a9cea
Marcos V. Piesske <mv.piesske@gmail.com>
2021-01-13 21:23:30 -0300

    Update index to new pages and endpoints

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ session_start();
 
 require __DIR__.'/vendor/autoload.php';
 
+use HcodeEcom\modules\repositories\UserRepository;
 use HcodeEcom\pages\Page;
 use HcodeEcom\pages\PageAdmin;
 
@@ -17,10 +18,40 @@ $app->get("/", function(){
 });
 
 $app->get("/admin", function(){
+
+   UserRepository::verifyLogin();
+   
    $page = new PageAdmin();
 
    $page->setTpl("index");
 
+});
+
+$app->get("/admin/login", function(){
+  
+   $page = new PageAdmin([
+      "header" => false,
+      "footer" => false
+   ]);
+
+   $page->setTpl("login");
+});
+
+$app->post('/admin/login', function(){
+   $userRepo = new UserRepository();
+
+   $userRepo->login($_POST['login'], $_POST['password']);
+
+   header('Location: /admin');
+
+   exit();
+});
+
+$app->get('/admin/logout', function(){
+   UserRepository::logout();
+
+   header("Location: /admin/login");
+   exit();
 });
 
 $app->run();
